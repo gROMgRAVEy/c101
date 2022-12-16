363aeed595c9cc79c4bbe8270f041cbd2f0791ca
gROMgRAVEy <grimmygumdrops@yahoo.com>
2022-12-16 05:06:01 -0600

    fix comment

--- bar2.c
+++ bar2.c
@@ -5,7 +5,7 @@ int points[3] = {'0','0','0'};
 #define v2 ('1' + '0' + '0')
 #define v3 ('1' + '1' + '0')
 #define v4 ('1' + '1' + '1')
-
+//define expression get evaluated during preprocessing
 
 //use pipes or any other way to enter in 1's and 0's
 //this will have a frquincey chart. char will be higher up the more 1's there is and lower down the more 0 there is.
@@ -16,8 +16,6 @@ int main()
         for(register int i = 0; (points[i] = getchar()) != EOF; i = (i + 1 < 3) ? i + 1 : 0) //it fit in a for loop >:D
 		switch(points[0]+points[1]+points[+2])
 		{
-			//i wanted to do something like `case static '0' + '0' + '0';` so it only gets evlauated once. maybe the c compiler dose that already?
-			//even then i think define would be better as it gets evaluated before exacutable is compiled? 
 			case v1:
 				printf("_");
 				break;
