28e7ac0eba2ca114412c17a8ac83540b34d13408
gROMgRAVEy <grimmygumdrops@yahoo.com>
2022-12-17 07:11:02 -0600

    removed spaces and orginzed layout

--- bar2.c
+++ bar2.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-
-int points[3] = {'0','0','0'};
-
 //use pipes or any other way to enter in 1's and 0's
 //this will have a frquincey chart. char will be higher up the more 1's there is and lower down the more 0 there is.
 //echo '110101010011010100000111110101110' | ./a.out
 //.---.-.-...---.-.__.-^^^--.--^-
+#include <stdio.h>
+
 int main() 
 {
+	int points[3] = {'0','0','0'};
+
         for(register int i = 0; (points[i] = getchar()) != EOF; i = (i + 1 < 3) ? i + 1 : 0) //it fit in a for loop >:D
 		switch(points[0]+points[1]+points[+2])
 		{
@@ -25,7 +25,6 @@ int main()
 				break;
 			case '1' + '1' + '1':
 				printf("^");
-				break;
 		}
 
         return 1;
